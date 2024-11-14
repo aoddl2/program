f972a6cee37b2c14537533b54e09fcdab07fc7ce
aoddl2 <141632331+aoddl2@users.noreply.github.com>
2024-11-14 17:41:49 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,7 @@
+select id, fish_name, length
+from fish_info a join fish_name_info b
+on a.fish_type = b.fish_type
+where (a.fish_type, length) in (select fish_type, max(length)
+                                    from fish_info
+                                    group by fish_type)
+order by id;
